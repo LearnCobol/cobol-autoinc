e4c3c665cdd39a69401c82a7309c3e2b6da0100b
Colin MacDonald <colin.macdonald@capitalone.com>
2015-06-16 20:17:20 -0400

    Tweaks to README and Vagrant files

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-= Cobol Autoinc
+# Cobol Autoinc
 
 Simplest marginally-useful web service imaginable: Auto-incrementing ID generator.
+
+The Vagrant VM will provision itself with all the tools you need. You'll need to ssh into it, check out this project there, and compile the code.
+Once you've compiled the `autoinc.cgi` file, you call it as a CGI script.
+The easiest thing is to put it in `/home/vagrant/public_html`, and call it as either
+
+    curl -si http://localhost/~vagrant/cobol/autoinc.cgi  # from the VM
+
+or
+
+    curl -si http://localhost:9080/~vagrant/cobol/autoinc.cgi  # from the host machine
 
 To compile:
 ```
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,9 +3,6 @@
 if curl -s -m 10 https://www.google.com/ > /dev/null ; then
     echo "Running 'apt-get update'. May take a while..."
     apt-get update
-    #echo "Installing Java and Postgres. May take a while..."
-    #apt-get install -y openjdk-7-jdk postgresql
-    #echo "export JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/'" >> /home/vagrant/.bashrc
 
     # Dev tools
     sudo apt-get install -y git
@@ -15,9 +12,6 @@ if curl -s -m 10 https://www.google.com/ > /dev/null ; then
 
     # Useful utils
     #sudo apt-get install -y htop
-
-    # Needed to build, but not to run
-    #sudo apt-get install -y git maven sbt
 else
     echo "Download test failed. Are you on Visigoth?"
     echo "You should probably delete this image and start over."
